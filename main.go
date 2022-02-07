9ab96b4b139dbe1e42b779006106d7c7468373b2
FunTester <FunTester@88.com>
2022-02-07 15:50:07 +0800

    修复BUG

--- main.go
+++ main.go
@@ -15,9 +15,7 @@ func init() {
 	os.Mkdir("./log/", 0766)
 	//os.Mkdir("./long/", 0666)
 	file := "./log/" + string(time.Now().Format("20060102")) + ".log"
-	println(file)
-	os.Create(file)
-	openFile, _ := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	openFile, _ := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
 	writer := io.MultiWriter(os.Stdout, openFile)
 	log.SetOutput(writer)
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Ldate)
