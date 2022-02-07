61ec7d28b356becf8419fd689d8797b661bf2466
FunTester <FunTester@88.com>
2022-02-07 15:49:16 +0800

    优化性能执行框架

--- execute/execute.go
+++ execute/execute.go
@@ -1,19 +1,25 @@
 package execute
 
 import (
+	"funtester/futil"
 	"log"
 	"sync"
 	"time"
 )
 
-func Execute(fun func(), times, thread int) {
+// ExecuteRoutineTimes
+// @Description: FunTester性能测试执行框架
+// @param fun 待执行方法
+// @param times 次数
+// @param thread 线程数
+func ExecuteRoutineTimes(fun func(), t, r int) {
 	c := make(chan int) //确认所有线程都结束
 	key := false        //用于控制所有线程一起结束
-	start := time.Now().UnixMilli()
-	for i := 0; i < thread; i++ {
+	start := futil.Milli()
+	for i := 0; i < r; i++ {
 		go func() {
 			sum := 0
-			for i := 0; i < times; i++ {
+			for i := 0; i < t; i++ {
 				if key {
 					break
 				}
@@ -25,11 +31,11 @@ func Execute(fun func(), times, thread int) {
 		}()
 	}
 	total := 0
-	for i := 0; i < thread; i++ {
+	for i := 0; i < r; i++ {
 		num := <-c
 		total += num
 	}
-	end := time.Now().UnixMilli()
+	end := futil.Milli()
 	diff := end - start
 	//total := thread * times
 	log.Printf("总耗时: %f", float64(diff)/1000)
@@ -37,7 +43,40 @@ func Execute(fun func(), times, thread int) {
 	log.Printf("请求总数: %d", total)
 	log.Printf("QPS: %f", float64(total)/float64(diff)*1000.0)
 }
+func ExecuteRoutineTime(fun func(), t, r int) {
+	c := make(chan int) //确认所有线程都结束
+	key := false        //用于控制所有线程一起结束
+	start := time.Now().UnixMilli()
+	go func() {
+		futil.Sleep(t * 1000)
+		key = true
+	}()
+	for i := 0; i < r; i++ {
+		go func() {
+			sum := 0
+			for {
+				if key {
+					break
+				}
+				fun()
+				sum++
+			}
+			c <- sum
+		}()
+	}
+	total := 0
+	for i := 0; i < r; i++ {
+		num := <-c
+		total += num
+	}
+	end := time.Now().UnixMilli()
+	diff := end - start
+	//total := thread * time
+	log.Printf("总耗时: %f", float64(diff)/1000)
 
+	log.Printf("请求总数: %d", total)
+	log.Printf("QPS: %f", float64(total)/float64(diff)*1000.0)
+}
 func ExecuteTask(task Task) {
 	c := make(chan int) //确认所有线程都结束
 	key := false        //用于控制所有线程一起结束
