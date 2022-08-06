173eea9f849e47a043419aff3eebcab81b66b31a
FunTester <FunTester@88.com>
2022-08-06 22:28:31 +0800

    修改常量的命名方式

--- base/constant.go
+++ base/constant.go
@@ -1,15 +1,15 @@
 package base
 
 const (
-	FunTester        = "FunTester"
-	Empty            = ""
-	TEST_ERROR       = -2
-	Comma            = ","
-	Space            = " "
-	LINE             = "\n\r"
-	Format           = "2006年01月02日 15时04分05秒"
-	Format_num       = "20060102150405"
-	ErrorInt         = -1
-	UserAgent        = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
-	Connection_Alive = "keep-alive"
+	FunTester       = "FunTester"
+	Empty           = ""
+	TestError       = -2
+	Comma           = ","
+	Space           = " "
+	LINE            = "\n\r"
+	Format          = "2006年01月02日 15时04分05秒"
+	FormatNum       = "20060102150405"
+	ErrorInt        = -1
+	UserAgent       = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+	ConnectionAlive = "keep-alive"
 )
